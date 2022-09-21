1109b0e0d7876c0173a5fcc042ddef6e263d9ce3
oem <oem@DESKTOP-R2RAV7A>
2022-09-21 17:20:41 +0900

    두번째 업로드

---
+++ pro11/src/main/webapp/hello.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>첫 번째 jsp 페이지</title>
+</head>
+<body>
+<h1>hello jsp!!</h1>
+<h1>jsp 실습입니다</h1>
+</body>
+</html>	
---
+++ pro24/src/main/java/com/spring/member/service/MemberService.java
@@ -0,0 +1,16 @@
+package com.spring.member.service;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+import com.spring.member.vo.MemberVO;
+
+public interface MemberService {
+	 public List listMembers() throws DataAccessException;
+	 public int addMember(MemberVO membeVO) throws DataAccessException;
+	 public int removeMember(String id) throws DataAccessException;
+	 public MemberVO findMember(String id) throws DataAccessException;
+	 public int modMember(MemberVO membeVO) throws DataAccessException;
+	
+
+}
